8cea94105e1cf1afcea407100c295f5969c2bd59
Jason Tungay <jtungay@gmail.com>
2020-02-25 20:09:02 +0000

    A 'nice' starting point. records audio, has a play/pause btns, havnt tested out the audio.read fully. Took this from github and have left my attempted version here in case I want to move to it.  The main prob I had was that I didnt do the file writting part cause i didnt think that I needed to

--- Synaethesia Sim/app/src/main/java/com/example/synaethesiasim/MainActivity.java
+++ Synaethesia Sim/app/src/main/java/com/example/synaethesiasim/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.synaethesiasim;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -10,12 +13,30 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.os.SystemClock;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Locale;
+
+import static javax.xml.transform.OutputKeys.ENCODING;
 
 public class MainActivity extends AppCompatActivity {
-    private int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private int AUDIO_SOURCE = MediaRecorder.AudioSource.MIC;
+    private int SAMPLE_RATE = 44100; // Hz
+    private int ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private int CHANNEL_MASK = AudioFormat.CHANNEL_IN_MONO;
+
+    private int BUFFER_SIZE = 2 * AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_MASK, ENCODING);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +54,33 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
+        int REQUEST_RECORD_AUDIO_PERMISSION = 200;
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO_PERMISSION);
+        }
+
+        startAudio();
+    }
+
+    private void startAudio() {
+        AudioRecord audioRecord = new AudioRecord(AUDIO_SOURCE, SAMPLE_RATE, CHANNEL_MASK, ENCODING, BUFFER_SIZE);
+
+        // Avoiding loop allocations
+        ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
+        int read;
+        String text = "";
+
+        TextView testText = findViewById(R.id.testText);
+
+        audioRecord.startRecording();
+        if (audioRecord.getState() == AudioRecord.STATE_INITIALIZED) {
+            read = audioRecord.read(buffer, BUFFER_SIZE);
+            for (int i = 0; i < BUFFER_SIZE; i++){
+                text += String.valueOf(buffer.array()[i]);
+            }
+            testText.setText(text);
+            text = "";
+            audioRecord.stop();
         }
     }
 
