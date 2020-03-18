f47ec7a241a58f028fa614d0998c9a0fd466eaa4
Jason Tungay <jtungay@gmail.com>
2020-03-18 19:19:22 +0000

    this is a WORKING MINIMAL VIABLE PRODUCT. I downloaded a free open source example from github of a chromatic tuner which uses tarsosdsp. I have edited the background colour to be the notes colour. e.g. C will make the entire background red

--- android-audiorecord-sample-master/src/main/java/com/example/audiorecord/AudioRecordActivity.java
+++ android-audiorecord-sample-master/src/main/java/com/example/audiorecord/AudioRecordActivity.java
@@ -31,6 +31,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.concurrent.atomic.AtomicBoolean;
+import org.jtransforms.fft.DoubleFFT_1D;
 
 /**
  * Sample that demonstrates how to record a device's microphone using {@link AudioRecord}.
@@ -158,8 +159,40 @@ public class AudioRecordActivity extends AppCompatActivity {
                     }
                     outStream.write(buffer.array(), 0, BUFFER_SIZE);
 
-                    test = String.valueOf(buffer.array()[10]);
+
+
+
+                    for (int i = 15; i < 30; i++){
+                        test += buffer.array()[i] + ",";
+                    }
                     textView.setText(test);
+                    test = "";
+
+                    DoubleFFT_1D fft = new DoubleFFT_1D(BUFFER_SIZE);
+                    // Create a byte array
+                    byte[] bytes = new byte[10];
+
+// Wrap a byte array into a buffer
+                    ByteBuffer buf = ByteBuffer.wrap(bytes);
+
+// Retrieve bytes between the position and limit
+// (see Putting Bytes into a ByteBuffer)
+                    bytes = new byte[buf.remaining()];
+
+// transfer bytes from this buffer into the given destination array
+                    buf.get(bytes, 0, bytes.length);
+
+// Retrieve all bytes in the buffer
+                    buf.clear();
+                    bytes = new byte[buf.capacity()];
+
+// transfer bytes from this buffer into the given destination array
+                    buf.get(bytes, 0, bytes.length);
+                    double[] dub = ByteBuffer.wrap(bytes).array();//.getDouble();
+                    double[] data = new double[BUFFER_SIZE];
+                    fft.complexForward(data);
+
+
 
                     buffer.clear();
                 }
