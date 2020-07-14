1153751cf16e8bfd85dbc923e65d736334acf7a5
Jason Tungay <jtungay@gmail.com>
2020-07-14 17:09:52 +0100

    fixed the sample rate issue, hopefully. Added an if statement that will run the sample rate checker if the sdk version is 20 or more. this worked on an android g20 (which is old and naff), will test on other phones next

--- Synaethesia/app/src/main/java/com/github/synaethesia/ListenerFragment.java
+++ Synaethesia/app/src/main/java/com/github/synaethesia/ListenerFragment.java
@@ -31,17 +31,21 @@ public class ListenerFragment extends Fragment {
     private PitchListener pitchListener;
 
     public void getValidSampleRates() {
-        for (int rate : new int[] {102000, 88200, 44100}) {  // add the rates you wish to check against
-            try {
-                int bufferSize = AudioRecord.getMinBufferSize(rate, AudioFormat.CHANNEL_CONFIGURATION_DEFAULT, AudioFormat.ENCODING_PCM_16BIT);
-                if (bufferSize > 0) {
-                    // buffer size is valid, Sample rate supported
-                    SAMPLE_RATE = rate;
+        if (Build.VERSION.SDK_INT >= 20) {
+            for (int rate : new int[] {102000, 88200, 44100}) {  // add the rates you wish to check against
+                try {
+                    int bufferSize = AudioRecord.getMinBufferSize(rate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+                    if (bufferSize > 0) {
+                        // buffer size is valid, Sample rate supported
+                        SAMPLE_RATE = rate;
+                        break;
+                    }
+                } catch (Exception e) {
                     break;
                 }
-            } catch (Exception e) {
-                break;
             }
+        } else {
+            SAMPLE_RATE = 44100;
         }
     }
 
